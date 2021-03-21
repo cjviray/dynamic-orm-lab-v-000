3c71dbee970c0d19c709514d1853b14dce60a618
Christian V <viray.christianj@gmail.com>
2021-03-21 02:32:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,9 +49,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
-
   end
   
+  def
   
 end
 
