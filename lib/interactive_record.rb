c1e2c2fa6c6aa9ecc245754e0d47c7a70e6d6dc3
Christian V <viray.christianj@gmail.com>
2021-03-21 02:46:16 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,7 @@ class InteractiveRecord
   end
   
   def self.find_by(attribute)
-    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]}) = ?"
+    "SELECT * FROM #{self.table_name} WHERE #{attribute.keys[0]}) = ?"
     
     
   end 
